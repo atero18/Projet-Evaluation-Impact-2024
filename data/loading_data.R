9877a260c5d25041eb0511d683f8d2db121b48be
Tangi Tassin <dev.ttassin@proton.me>
2024-11-23 18:02:28 +0100

    Add main data (exp & non-exp)

---
+++ data/loading_data.R
@@ -0,0 +1,47 @@
+library(readr)
+library(dplyr)
+
+readFile <- function(file)
+{
+  # The variables from left to right are: 
+  # treatment indicator (1 if treated, 0 if not treated), 
+  # age, education, Black (1 if black, 0 otherwise), 
+  # Hispanic (1 if Hispanic, 0 otherwise), married (1 if married, 0 otherwise), 
+  # nodegree (1 if no high school degree, 0 otherwise), 
+  # RE74 (earnings in 1974), RE75 (earnings in 1975),
+  # and RE78 (earnings in 1978)
+
+  colTypes <- cols("T" = "f", 
+                   age = "i", education = "i", 
+                   Black = "f", Hispanic = "f", 
+                   married = "f", nodegree = "f", 
+                   RE74 = "d", RE75 = "d", RE78 = "d")
+  
+  data <- readr::read_csv(file, col_names = TRUE, col_types = colTypes)
+  
+  # Suggestion from Rubin & Imbens (2015)
+  # indRE74 (1 if earnings in 1974, 0 otherwise)
+  # indRE75 (1 if earnings in 1975, 0 otherwise)
+  data %>% 
+    mutate(indRE74 = (RE74 > 0.0) %>% as.integer() %>% factor(),
+           indRE75 = (RE75 > 0.0) %>% as.integer() %>% factor())
+  
+}
+
+controlExp <- readFile("nswre74_Dehejia_Wahha_exp_control.csv")
+
+treatedExp <- readFile("nswre74_Dehejia_Wahha_exp_treated.csv")
+
+dataExp <- rbind(controlExp, treatedExp)
+
+rm(controlExp)
+
+summary(dataExp)
+
+controlObs <- readFile("cps_nonexp_control.csv")
+
+dataObs <- rbind(controlObs, treatedExp)
+
+rm(treatedExp, controlObs)
+
+summary(dataObs)
